7c409c6dafac01de0ed32f647ded3844259a064a
ydrubs <ydrubins@yahoo.com>
2024-08-12 10:17:58 -0500

    Added score list

--- Unit 0 - Fundementals/Exercises/0.1 - List Practice 2.py
+++ Unit 0 - Fundementals/Exercises/0.1 - List Practice 2.py
@@ -4,3 +4,4 @@ Given a list of 5 scores, for example [34,12,2,76] , ask the user for a numeric 
 Write a program that places the entered score in the correct position of the list, drops the lowest score, and prints the resulting list.
 
 """
+scores = [34,12,2,76]
