f4e50357ec84f248a24e98f738240ef73fc3a467
ydrubs <ydrubins@yahoo.com>
2024-08-12 09:56:13 -0500

    First Commit

---
+++ Unit 0 - Fundementals/Exercises/0.1 - List Practice 2.py
@@ -0,0 +1,6 @@
+"""
+Given a list of 5 scores, for example [34,12,2,76] , ask the user for a numeric input.
+
+Write a program that places the entered score in the correct position of the list, drops the lowest score, and prints the resulting list.
+
+"""
---
+++ Unit 0 - Fundementals/Exercises/0.1 - List Practice1.py
@@ -0,0 +1,34 @@
+"""
+Write a program that asks the user to enter decimal numbers.
+
+The numbers are added to a list until an empty string is recieved.
+
+Once an empty string is recieved, the program calculates and prints the average of the numbers in the list
+"""
+
+# Reminder 1 - While loops
+
+
+
+# Reminder 2 - Recasting and recieving an empty string
+
+
+
+
+
+# Reminder 3 - Using an f-string
+
+
+
+
+
+# Reminder 4 - the len and sum command
+
+
+
+
+
+
+"""
+My Solution
+"""
