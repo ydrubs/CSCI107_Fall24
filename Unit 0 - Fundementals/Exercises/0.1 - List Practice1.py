70dfca1695d1b4952febe8eacdd011ccd3c3eee4
ydrubs <ydrubins@yahoo.com>
2024-08-23 16:02:03 -0500

    8/23 update

--- Unit 0 - Fundementals/Exercises/0.1 - List Practice1.py
+++ Unit 0 - Fundementals/Exercises/0.1 - List Practice1.py
@@ -7,26 +7,42 @@ Once an empty string is recieved, the program calculates and prints the average 
 """
 
 # Reminder 1 - While loops
-
+# countdown = 10
+# while countdown > -1:
+#     print(countdown)
+#     countdown -=1
 
 
 # Reminder 2 - Recasting and recieving an empty string
-
+# value = None
+#
+# while True:
+    # data = input('Enter a number: ')
+#     #If nothing is entered
+#     if data == '':
+#         break
+#     else:
+#         print(data)
 
 
 
 
 # Reminder 3 - Using an f-string
+first_name = 'john'
+last = 'Doe'
 
-
+# print(f'Hi {first_name} {last}')
 
 
 
 # Reminder 4 - the len and sum command
+numbers = [1,2,3,4,5, 46]
 
+print(len(numbers))
+print(sum(numbers))
 
-
-
+import statistics
+print(statistics.mean(numbers))
 
 
 """
