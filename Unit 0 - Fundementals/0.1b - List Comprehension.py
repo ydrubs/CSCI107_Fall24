125733e8e526656e7ccddf83175a3d768597376c
ydrubs <ydrubins@yahoo.com>
2024-08-27 13:42:07 -0500

    8/27 update

---
+++ Unit 0 - Fundementals/0.1b - List Comprehension.py
@@ -0,0 +1,37 @@
+"""0.1b - List Comprehension"""
+
+
+"""Create a list of values from 0 to 99 using a for loop"""
+
+
+"""Use list comprehension to acheive the same thing"""
+
+
+"""Filtering - Create a list of only even numbers between 0 and 99 using a for loop"""
+
+
+"""Filtering - Create a list of only even numbers between 0 and 99 using list comprehension"""
+
+
+"""Mapping - Create a list that doubles every number between 0 and 99 using a for loop"""
+
+
+"""Mapping - Create a list that doubles every number between 0 and 99 using list comprehension"""
+
+
+"""Combining mapping and filtering with a loop.
+Triple all the even numbers between 1 and 100"""
+
+
+"""Combining mapping and filtering with list comprehension
+Triple all the even numbers between 1 and 100"""
+
+
+"""Using list comprehension to filter common elements of two lists
+First we'll make two lists - multiples of 3 and 5 up tp 100 using list comprehension"""
+
+
+"""Now we use a for loop to filter the common elements in both lists and write to a new list"""
+
+
+"""...using list comprehension to do the same"""
---
+++ Unit 0 - Fundementals/Exercises/0.2 - List Comprehension Practice.py
@@ -0,0 +1,27 @@
+"""Exercise 0.2 - List Comprehension"""
+
+"""
+Problem 1 -
+
+Given the text in the variable text, use list comprehension to return a list of all words in the text
+    that don't contain the word 'the'. NOTE - It should not be case-sensitive, so both 'the' and 'The'
+    should be removed.
+"""
+
+text = """The mouse ran up the clock. 
+           The clock struck one, the mouse ran down. 
+           The clock struck noon, the mouse ran up."""
+
+###YOUR SOLUTION HERE
+
+
+"""
+Problem 2 - 
+
+Given the list of names. Use list comprehension to assign a random integer between 1 and 20 to each person. 
+NOTE - The new list should have only integers and a lenght that is the same as the lenght of the list with names. 
+"""
+from random import randint
+names = ['Tom', 'Bob', 'Frank', 'Ron', 'Jon']
+
+###YOUR SOLUTION HERE
