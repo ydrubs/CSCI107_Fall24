e19d15e3c29cf094507b4a028499b080dbba1c83
ydrubs <ydrubins@yahoo.com>
2024-08-28 11:29:58 -0500

    first commit

---
+++ Unit 0 - Fundementals/Exercises/0.1 - List Practice 3 (extra).py
@@ -0,0 +1,9 @@
+"""Find the most frequent element in a list and print it out.
+
+- If all the elements occur once - print NONE
+- If two elements are tied for the most frequent, print one of them (optional - print all of them)
+
+"""
+
+lst = [1,2,2,2,3,3,4,5]
+print(lst.count(2))
