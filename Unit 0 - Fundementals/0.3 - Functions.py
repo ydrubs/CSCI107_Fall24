a4f9066d69bef61b300f02df06f016bafa7cbbd6
ydrubs <ydrubins@yahoo.com>
2024-09-04 10:39:31 -0500

    initial commit

---
+++ Unit 0 - Fundementals/0.3 - Functions.py
@@ -0,0 +1,52 @@
+#Declaring a function
+
+
+
+
+###########################
+
+#Declaring a function with a parameter
+
+
+
+
+###########################
+
+
+#Declaring a function with multiple parameters
+
+
+
+#An error occurs if we call the function without the same number of parameters it expects
+
+
+
+###########################
+
+
+#Positional vs. keyword parameters
+# Keyword parameters do not have to be passed in
+#... They take the default value when not passed in
+
+
+
+
+
+###########################
+
+
+#A function, by default returns the data type NONE
+# In order to have it return back any other data, we need to use the 'return command'
+# This is good practice and often prefered over print()
+
+     #Does not show anything
+     #Print the return from the function
+
+    #save output from function to a variable
+
+
+###########################
+
+#Functions can call other functions
+
+
