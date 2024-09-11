82d139fb2efc57d1abb635aa61efd77ee6a15cea
ydrubs <ydrubins@yahoo.com>
2024-09-11 10:19:06 -0500

    initial commit

---
+++ Unit 0 - Fundementals/Exercises/0.3 - Function Exercise 1_ Chasing Collatz.py
@@ -0,0 +1,23 @@
+"""
+The Collatz number is a famous unsolved problem in math that can be easily explained with three simple rules.
+
+From a starting number, n, follow three rules to get the next number in the sequence:
+1)	If n is even, the next number n is n / 2.
+2)	If n is odd, the next number n is n * 3 + 1.
+3)	If n is 1, stop. Otherwise, repeat.
+
+Example: Start with n = 26. If you follow the rules, the sequence of number would be:
+    	26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1
+
+Your task:
+1)	Ask the user to enter a starting integer greater than 0 or QUIT to exit the program
+
+2)	Validate that the integer is valid (not a float, letter, or special symbol)
+
+3)	Write a function called collatz_sequence that can return a list representing the sequence that follows the Collatz rules for n.
+
+4)	For example by typing in collatz_sequence(number), where number = 100, you should get:
+        [100, 50, 25, 76, 38, 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+
+"""
+##YOUR SOLUTION HERE
