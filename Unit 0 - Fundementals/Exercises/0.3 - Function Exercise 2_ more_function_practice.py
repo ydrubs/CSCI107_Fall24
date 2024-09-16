3f6fed1a2df6e3b1bf17c41f335133a91ed77505
ydrubs <ydrubins@yahoo.com>
2024-09-16 08:57:48 -0500

    initial commit

---
+++ Unit 0 - Fundementals/Exercises/0.3 - Function Exercise 2_ more_function_practice.py
@@ -0,0 +1,27 @@
+"""
+Use functions to solve each exercise
+"""
+
+"""
+Exercise 1 - Capital indexes
+
+Write a function named capital_indexes. The function takes a single parameter, which is a string. 
+Your function should return a list of all the indexes in the string that have capital letters.
+For example, calling capital_indexes("HeLlO") should return the list [0, 2, 4].
+
+"""
+some_string = "hOW aRe YoU TOdaY"
+####Your solution here
+
+
+
+
+"""
+Exercise 2 - Prime Checker
+
+Write a function that excepts a positive integer greater then 1 as an input and returns if that number is prime or not.
+
+You should also include a validity checker somewhere in your program to check that the input is valid 
+(a positive integer greater then one is entered))
+"""
+####Your solution here
