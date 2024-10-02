17649a5d65a313cdc642ae0a6aa6bdfd30e5f315
ydrubs <ydrubins@yahoo.com>
2024-10-02 11:28:16 -0500

    initial commit

---
+++ Unit 1 - Object Oriented Programming/1_Defining Classes and Methods/vehicle1.py
@@ -0,0 +1,10 @@
+"""
+In this program we:
+
+1) Create a car class
+2) Define an initialization method with no parameters
+3) Define attributes for the class
+4) Define methods for the class without any functionality (yet)
+5) Test the class by importing it into another script (car lot) and creating an object from it
+"""
+
---
+++ Unit 1 - Object Oriented Programming/1_Defining Classes and Methods/vehicle2.py
@@ -0,0 +1,8 @@
+"""
+In this program we:
+
+Develop the concept of encapsulation by -
+        passing in parameters into the inititalization method rather than
+        allowing the object to define them
+
+"""
