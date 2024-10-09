5e12fbb0a08fc98a19b88e5e20090e1885395a06
ydrubs <ydrubins@yahoo.com>
2024-10-09 11:27:26 -0500

    initial commit

---
+++ Unit 1 - Object Oriented Programming/Exercises/Exercise 1.1 - Salad With Classes.py
@@ -0,0 +1,7 @@
+"""
+Create a class for making a salad.
+
+Include the attributes for the salad, include some (or all) that are passed into the initialization method.
+
+Include the methods that the salad might need in the class (you don't need to implement the methods)
+"""
---
+++ Unit 1 - Object Oriented Programming/Exercises/Exercise 1.2 - Dice Bag.py
@@ -0,0 +1,31 @@
+"""
+Create a class called DiceBag.
+
+In the initialization method (__init__), it should accept the number of sides
+desired and the number of dice avaialble to roll and set those as attributes.
+
+For example, we can have a bag of 10 six-sided dice.
+
+Add 3 methods:
+    - A method that allows you to remove up to as many dice as there are in the bag.
+      If more are removed the program does not allow it and gives a message
+
+    - A method to roll up to as many dice as there are in the bag and return the results of each.
+      Again, if more are rolled the program does not allow it and gives a message
+
+    - A method to add dice to the bag
+
+
+"""
+import random
+
+
+
+###Some possible methods for testing (Don't have to use)
+if __name__ == '__main__':
+    # bag = DiceBag(6,10)
+    # bag.add_dice(3)
+    # print(bag.number)
+    #
+    # roll = bag.roll(4)
+    # print(roll)
